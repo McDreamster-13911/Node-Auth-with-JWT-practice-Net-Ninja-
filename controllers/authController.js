37400b1b0a537898419214b702c80fdf172f5979
Ibrahim <the.ibrahimkhan.mik3301@gmail.com>
2024-12-22 22:31:14 +0530

    Created User Model and its schema and modified some controller functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+const User = require("../models/User");
+
+
 module.exports.signup_get = (req,res) => {
     res.render("Signup");
 }
@@ -6,14 +9,19 @@ module.exports.login_get = (req,res) => {
     res.render("Login");
 }
 
-module.exports.signup_post = (req,res) => {
+module.exports.signup_post = async (req,res) => {
     const { email, password } = req.body;
 
-    console.log(email, password)
-    res.send("New Signup");
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("Error, user not created");
+    }
 }
 
-module.exports.login_post = (req,res) => {
+module.exports.login_post = async (req,res) => {
     const { email, password } = req.body;
 
     console.log(email, password)
