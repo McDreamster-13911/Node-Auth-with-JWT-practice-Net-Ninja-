742b53ca111fd2f824d7a533d51869632c1c8f1b
Ibrahim <the.ibrahimkhan.mik3301@gmail.com>
2024-12-25 23:20:45 +0530

    Implemented the functionality for jwt

--- app.js
+++ app.js
@@ -34,22 +34,3 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
-// cookies
-app.get("/set-cookies", (req,res) => {
-
-  // Setting the cookie
-  // res.setHeader("Set-Cookie", "newUser=true");
-
-  res.cookie("newUser", false);
-  res.cookie("isEmployee", true, { maxAge : 1000*60*60*24, httpOnly:true});
-  res.send("You got the cookies");
-  
-});
-
-app.get("/read-cookies", (req,res) => {
-
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-
-  res.json(cookies);
-});
