3d087f65fc2db0ddf86fed439b9846d2176c0f92
Ibrahim <the.ibrahimkhan.mik3301@gmail.com>
2024-12-29 20:31:39 +0530

    Added conditional rendering of elements using ejs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const authRoutes = require("./routes/authRoutes")
 const cookieParser = require("cookie-parser");
-const {requireAuth } = require("./middleware/authMiddleware");
+const {requireAuth, checkUser } = require("./middleware/authMiddleware");
 
 const app = express();
 
@@ -31,6 +31,7 @@ mongoose.connect(dbURI, {
   .catch((err) => console.error('Database connection error:', err));
 
 // routes
+app.get("*", checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
